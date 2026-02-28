162313fb7c4a11b6590e12ecf4d8b157dc5c17f0
Manmeet Singh <meetsingh459@gmail.com>
2026-02-28 12:25:48 +0530

    Added NewItem api support

--- ToDoList/Models/ToDoListItem.swift
+++ ToDoList/Models/ToDoListItem.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct ToDoListItem: Codable, Identifiable {
+    var id: String
+    var title: String
+    var isCompleted: Bool
+    var dueDate: TimeInterval
+    var createdDate: TimeInterval
+    
+    mutating func setCompleted(_ state: Bool) {
+        isCompleted = state
+    }
+}
--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import Combine
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel : ObservableObject {
     
@@ -31,6 +33,40 @@ class NewItemViewViewModel : ObservableObject {
     }
     
     func saveItem() {
+        guard canSave, let userID = Auth.auth().currentUser?.uid else {
+            return
+        }
         
+        let db = Firestore.firestore()
+        let newId = UUID().uuidString
+        let model = ToDoListItem(
+            id: newId,
+            title: title,
+            isCompleted: false,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970
+        )
+
+        do {
+            let data = try model.asDictionary()
+            db.collection("users")
+                .document(userID)
+                .collection("todos")
+                .document(newId)
+                .setData(data) { [weak self] error in
+                    if let error = error {
+                        print("Failed to save item: \(error.localizedDescription)")
+                        DispatchQueue.main.async {
+                            self?.showAlert = true
+                        }
+                    }
+                }
+        } catch {
+            print("Failed to encode model to dictionary: \(error)")
+            DispatchQueue.main.async { [weak self] in
+                self?.showAlert = true
+            }
+        }
     }
 }
+
